cd9a1912033e3307447b0fb5b4ec25e29230ae9f
Abdulsamet Karakaya <56205106+developerkarakaya@users.noreply.github.com>
2020-11-27 23:29:59 +0300

    C# Barkod ve QR Kod Project Added

---
+++ barkodUygulama/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+
+namespace barkodUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder ENC= new QRCodeEncoder();
+            pictureBox1.Image = ENC.Encode(textBox1.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code128BarcodeDraw brc = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = brc.Draw(textBox1.Text, 35);
+        }
+    }
+}
